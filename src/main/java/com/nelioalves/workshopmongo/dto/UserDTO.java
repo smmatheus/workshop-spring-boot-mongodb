0af85ffb174061d2d5a5403d08d838ad213ff911
Matheus Morais <matheusssm01@gmail.com>
2024-05-16 09:14:09 -0300

    Obtendo um usuário por id

--- src/main/java/com/nelioalves/workshopmongo/dto/UserDTO.java
+++ src/main/java/com/nelioalves/workshopmongo/dto/UserDTO.java
@@ -16,7 +16,7 @@ public class UserDTO implements Serializable {
 	}
 	
 	public UserDTO(User obj) {
-		id = obj.getEmail();
+		id = obj.getId();
 		name = obj.getName();
 		email = obj.getEmail();
 	}
